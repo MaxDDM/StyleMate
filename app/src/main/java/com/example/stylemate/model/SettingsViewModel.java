1943646ad40fc5c4e837766508c4575afe720853
Konstantin <pupkov.kostya.bel@mail.ru>
2026-02-22 11:58:22 +0300

    change data use in settings

--- app/src/main/java/com/example/stylemate/model/SettingsViewModel.java
+++ app/src/main/java/com/example/stylemate/model/SettingsViewModel.java
@@ -2,13 +2,15 @@ package com.example.stylemate.model; // Пакет model
 
 import android.content.Context;
 
+import android.app.Application;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.AndroidViewModel;
 
 import com.example.stylemate.repository.UserRepository;
 
-public class SettingsViewModel extends ViewModel {
+public class SettingsViewModel extends AndroidViewModel {
 
     private final UserRepository repository = new UserRepository();
 
@@ -20,12 +22,15 @@ public class SettingsViewModel extends ViewModel {
     private final MutableLiveData<Boolean> _logoutEvent = new MutableLiveData<>();
     public LiveData<Boolean> logoutEvent = _logoutEvent;
 
-    public SettingsViewModel(Context context) {
-        loadData(context);
+    public SettingsViewModel(Application application) {
+        super(application);
+        // Сразу загружаем данные
+        loadData();
     }
 
-    private void loadData(Context context) {
-        UserProfile profile = repository.getUserProfile(context);
+    private void loadData() {
+        // 3. getApplication() — это тот самый Context, который ты искал
+        UserProfile profile = repository.getUserProfile(getApplication());
         _userProfile.setValue(profile);
     }
 
