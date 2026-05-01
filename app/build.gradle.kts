f150bb0e6140c640577fdf9a9b166e334608fc88
Konstantin <pupkov.kostya.bel@mail.ru>
2026-05-01 18:01:40 +0300

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.pupkov.stylemate"
         minSdk = 31
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
